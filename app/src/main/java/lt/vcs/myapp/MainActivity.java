b13f4e1b7cc9b0b13fa3251a24dcf50b98f113a3
PC <daniellukasevic92@gmail.com>
2022-01-16 16:22:11 +0200

    Add Click Metodh

--- app/src/main/java/lt/vcs/myapp/MainActivity.java
+++ app/src/main/java/lt/vcs/myapp/MainActivity.java
@@ -3,10 +3,11 @@ package lt.vcs.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button buttonTic;
     Button buttonSnake;
     Button buttonPuzzle;
@@ -20,5 +21,22 @@ TextView txtView;
         buttonSnake = (Button) findViewById(R.id.btn2);
         buttonPuzzle = (Button) findViewById(R.id.btn3);
         txtView = (TextView) findViewById(R.id.txt);
+        buttonTic.setOnClickListener(this);
+        buttonSnake.setOnClickListener(this);
+        buttonPuzzle.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.btn1){
+            txtView.setText("FirstButton is clicked");
+        }
+        if(view.getId()==R.id.btn2){
+            txtView.setText("SecondButton is clicked");
+        }
+        if(view.getId()==R.id.btn3){
+            txtView.setText("ThirdButton is clicked");
+        }
     }
 }
