a75e333234bff157528de452d282d90235c10fe9
PC <daniellukasevic92@gmail.com>
2022-01-18 20:35:46 +0200

    Add onLongClick

--- app/src/main/java/lt/vcs/myapp/MainActivity.java
+++ app/src/main/java/lt/vcs/myapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnLongClickListener {
     Button buttonTic;
     Button buttonSnake;
     Button buttonPuzzle;
@@ -21,22 +21,35 @@ TextView txtView;
         buttonSnake = (Button) findViewById(R.id.btn2);
         buttonPuzzle = (Button) findViewById(R.id.btn3);
         txtView = (TextView) findViewById(R.id.txt);
-        buttonTic.setOnClickListener(this);
-        buttonSnake.setOnClickListener(this);
-        buttonPuzzle.setOnClickListener(this);
+        buttonTic.setOnLongClickListener(this);
+        buttonSnake.setOnLongClickListener(this);
+        buttonPuzzle.setOnLongClickListener(this);
 
     }
+//@Override
+//    public void onLongClickItem(View view) {
+//        if(view.getId()==R.id.btn1){
+//            txtView.setText("Tic-Tac-Toe Button is clicked");
+//        }
+//        if(view.getId()==R.id.btn2){
+//            txtView.setText("Snake Button is clicked");
+//        }
+//        if(view.getId()==R.id.btn3){
+//            txtView.setText("Puzzle Button is clicked");
+//        }
+//    }
 
     @Override
-    public void onClick(View view) {
+    public boolean onLongClick(View view) {
         if(view.getId()==R.id.btn1){
             txtView.setText("Tic-Tac-Toe Button is clicked");
-        }
-        if(view.getId()==R.id.btn2){
-            txtView.setText("Snake Button is clicked");
-        }
-        if(view.getId()==R.id.btn3){
-            txtView.setText("Puzzle Button is clicked");
-        }
+      }
+       if(view.getId()==R.id.btn2){
+           txtView.setText("Snake Button is clicked");
+       }
+       if(view.getId()==R.id.btn3){
+           txtView.setText("Puzzle Button is clicked");
+       }
+       return false;
     }
 }
