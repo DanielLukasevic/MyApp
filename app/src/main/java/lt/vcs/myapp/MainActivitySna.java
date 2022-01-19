c7988e435bc20070313f29acd0fa56cd58649874
PC <daniellukasevic92@gmail.com>
2022-01-19 12:32:37 +0200

    Add SnaActivity

---
+++ app/src/main/java/lt/vcs/myapp/MainActivitySna.java
@@ -0,0 +1,20 @@
+package lt.vcs.myapp;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import lt.vcs.myapp.databinding.ActivityMainSnaBinding;
+
+public class MainActivitySna extends AppCompatActivity {
+
+}
