1a810a494c35fa0ed95e00bdb43e23ea01a726ba
PC <daniellukasevic92@gmail.com>
2022-01-19 08:05:30 +0200

    Add Main2ActivityTic

---
+++ app/src/main/java/lt/vcs/myapp/Main2ActivityTic.java
@@ -0,0 +1,14 @@
+package lt.vcs.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2ActivityTic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
